f75c76957a6aa8a7807aacfc6f6fbc1ff2b04687
Hanuvendra <35001230+Hanuvendra@users.noreply.github.com>
2018-03-29 10:55:38 +0530

    Update SmallestNumberofNotes.py

--- Beginner-problens/SmallestNumberofNotes.py
+++ Beginner-problens/SmallestNumberofNotes.py
@@ -3,22 +3,22 @@ for i in range(t):
     n=int(input())
     count=0
     while(n>0):
-        if(n%100==0):
-            count+=1
-            n=n-100
-        elif(n%50==0):
-            count+=1
-            n=n-50
-        elif(n%10==0):
-            count+=1
-            n=n-10
-        elif(n%5==0):
-            count+=1
-            n=n-5
-        elif(n%2==0):
-            count+=1
-            n=n-2
-        elif(n%1==0):
-            count+=1
-            n=n-1
+        if(n>=100):
+            count+=(n//100)
+            n=n-((n//100)*100)
+        elif(n>=50):
+            count+=(n//50)
+            n=n-((n//50)*50)
+        elif(n>=10):
+            count+=(n//10)
+            n=n-((n//10)*10)
+        elif(n>=5):
+            count+=(n//5)
+            n=n-((n//5)*5)
+        elif(n>=2):
+            count+=(n//2)
+            n=n-((n//2)*2)
+        elif(n>=1):
+            count+=(n//1)
+            n=n-((n//1)*1)
     print(count)
